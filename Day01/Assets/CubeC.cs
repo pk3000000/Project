ee0d182e691fa9ba561b26f5e0d6f57e1532df54
pk3000000 <pk3000000@nate.com>
2017-04-03 16:03:45 +0900

    bbc

---
+++ Day01/Assets/CubeC.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeC : MonoBehaviour {
+
+    public float abc;
+    private int bbc;
+
+	// Use this for initialization
+	void Start () {
+        abc = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W) || Input.GetMouseButton(0)) // 누르는 중
+        {
+            gameObject.transform.rotation *= Quaternion.Euler(10, 0, 0);
+            gameObject.transform.position += new Vector3(abc, 0.0f, 0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetMouseButton(1))
+        {
+            gameObject.transform.rotation *= Quaternion.Euler(-10, 0, 0);
+            gameObject.transform.position += new Vector3(abc, 0.0f, -0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            abc -= 0.01f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            abc += 0.01f;
+        }
+
+        if(Input.GetKeyUp(KeyCode.A))
+        {
+            abc = 0.0f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            abc = 0.0f;
+        }
+
+        Debug.Log(Input.mousePosition);
+
+        // Input.GetKeyDown() // 한번
+        // Input.GetKeyUp()   // 한번
+    }
+}
---
+++ Day01/Assets/CubeControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControl : MonoBehaviour {
+
+    public int publicVar = 0;
+    private int privateVar = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Update()");
+	}
+}
