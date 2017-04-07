73e38a02f56b490b1b20f5e4c9340de43692368b
pk3000000 <pk3000000@nate.com>
2017-04-07 15:39:46 +0900

    aaa

---
+++ Day05/Assets/script/Touch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour {
+
+    Animator ani;
+    GameObject hitObject;
+
+	// Use this for initialization
+	void Start () {
+        ani = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray;
+        RaycastHit hit;
+
+        ani.SetBool("Touch", false);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray,out hit, 100))
+            {
+                hitObject = hit.collider.gameObject;
+                if(hitObject.gameObject.tag == "Head")
+                {
+                    ani.SetBool("Touch", true);
+                }
+                else
+                {
+                    Debug.Log("Hit");
+                }
+            }
+        }
+	}
+}
---
+++ Day5/Assets/Script/CameraControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    GameObject cameraParent;
+
+    Vector3 defaultPosition;
+    Quaternion defaultRotation;
+    float defaultZoom;
+
+	// Use this for initialization
+	void Start () {
+        cameraParent = GameObject.Find("CameraParent");
+
+        defaultPosition = Camera.main.transform.position;
+        defaultRotation = cameraParent.transform.rotation;
+        defaultZoom = Camera.main.fieldOfView;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0))
+        {
+            Camera.main.transform.Translate(-Input.GetAxisRaw("Mouse X") / 10,
+                                            -Input.GetAxisRaw("Mouse Y") / 10,
+                                            0
+            );  // 이동
+        }
+
+        if(Input.GetMouseButton(1))
+        {
+            // Camera.main.transform.Rotate(-Input.GetAxisRaw("Mouse Y") * 10,
+            //                              -Input.GetAxisRaw("Mouse X") * 10,
+            //                              0
+            //   );
+
+            cameraParent.transform.Rotate(-Input.GetAxisRaw("Mouse Y") * 10,
+                                          -Input.GetAxisRaw("Mouse X") * 10,
+                                          0
+            );  // 회전
+        }
+
+        Camera.main.fieldOfView += (20 * Input.GetAxis("Mouse ScrollWheel"));
+
+        if(Camera.main.fieldOfView < 10)
+        {
+            Camera.main.fieldOfView = 10;
+        }
+
+        if(Input.GetMouseButton(2))
+        {
+            Camera.main.transform.position = defaultPosition;
+            cameraParent.transform.rotation = defaultRotation;
+            Camera.main.fieldOfView = defaultZoom;
+        }
+	}
+}
