522298772f5692c12b0327c2a4d5915b5dc2da69
pk3000000 <pk3000000@nate.com>
2017-04-11 16:55:44 +0900

    abd

--- Day06/Assets/Bullet.cs
+++ Day06/Assets/Bullet.cs
@@ -6,18 +6,26 @@ public class Bullet : MonoBehaviour {
 
     private static readonly float bulletMoveSpeed = 10.0f;
     public GameObject hitEffectPrefab = null;
-    
+    Rigidbody rb;
     Vector3 vecAddPos;
+    Vector3 dir;
 
     // Use this for initialization
     void Start () {
-       
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        vecAddPos = (Vector3.forward * bulletMoveSpeed);
-        transform.position += ((transform.rotation * vecAddPos)*Time.deltaTime);
+        rb = GetComponent<Rigidbody>();
+        dir = Vector3.zero;
+        Camera[] subCamera = Camera.allCameras;
+        dir = subCamera[1].transform.forward;
+        vecAddPos = (dir * bulletMoveSpeed);
+        rb.AddForce(vecAddPos,ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //transform.Translate(transform.forward * Time.deltaTime);
+        //rb.AddForce(vecAddPos,ForceMode.VelocityChange);
+        //transform.position += ((vecAddPos)*Time.deltaTime);
+
     }
 
     private void OnCollisionStay(Collision collision)
@@ -27,5 +35,6 @@ public class Bullet : MonoBehaviour {
             Instantiate(hitEffectPrefab, transform.position, transform.rotation);
         }
         Destroy(gameObject);
+        
     }
 }
--- Day06/Assets/UniChan.cs
+++ Day06/Assets/UniChan.cs
@@ -10,6 +10,8 @@ public class UniChan : MonoBehaviour {
     public GameObject bulletObject = null;
     public Transform bulletStartPosition = null; // 총구 위치
 
+    GameObject shoulder;
+
     private static readonly float MOVE_Z_FRONT = 3.0f;
     private static readonly float MOVE_Z_BACK = -2.0f;
 
@@ -18,19 +20,30 @@ public class UniChan : MonoBehaviour {
 
     private static readonly float ROTATION_Y_KEY = 360.0f;
     private static readonly float ROTATION_Y_MOUSE = 360.0f;
-
+    private static readonly float ROTATION_X_KEY = -360.0f;
+    private static readonly float ROTATION_X_MOUSE = -360.0f;
     private float m_rotationY = 0.0f;
+    private float m_rotationX = 0.0f;
+    bool jumpFlag = false;
+    int jumpCount;
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        jumpCount = 0;
+        shoulder = GameObject.Find("Character1_Spine");
+    }
+
+    // Update is called once per frame
+    void Update () {
         CheckMouseLock();
         CheckMove();
 	}
+
+    private void LateUpdate()
+    {
+        //shoulder.transform.rotation = Quaternion.Euler(m_rotationX, 0, 0);
+        shoulder.transform.localEulerAngles = new Vector3(0, 0, m_rotationX);
+    }
 
     private void CheckMouseLock()
     {
@@ -70,8 +83,46 @@ public class UniChan : MonoBehaviour {
                 addRotationY += (Input.GetAxis("Mouse X") * ROTATION_Y_MOUSE);
             }
             m_rotationY += (addRotationY * Time.deltaTime);
+           // transform.rotation = Quaternion.Euler(0, m_rotationY, 0);
+        }
+        // 상체 회전량
+        {
+            float addRotationX = 0.0f;
+
+            if (m_mouseLockFlag)
+            {
+                if(m_rotationX < 80 && m_rotationX > 0)
+                {
+                    addRotationX -= (Input.GetAxis("Mouse Y") * ROTATION_X_MOUSE);
+
+                    m_rotationX += (addRotationX * Time.deltaTime);
+                 
+                }
+                else if(m_rotationX>-80 && m_rotationX < 0)
+                {
+                    addRotationX -= (Input.GetAxis("Mouse Y") * ROTATION_X_MOUSE);
+
+                    m_rotationX += (addRotationX * Time.deltaTime);
+                 
+                }
+                else if(m_rotationX == 0)
+                {
+                    addRotationX += (Input.GetAxis("Mouse Y") * ROTATION_X_MOUSE);
+
+                    m_rotationX += (addRotationX * Time.deltaTime);
+                }
+                else if(m_rotationX >= 80)
+                {
+                    m_rotationX = 79;
+                }
+                else if(m_rotationX <= -80)
+                {
+                    m_rotationX = -79;
+                }
+            }
             transform.rotation = Quaternion.Euler(0, m_rotationY, 0);
         }
+
         bool shiftFlag = false;
 
         Vector3 addPosition = Vector3.zero;
@@ -79,7 +130,7 @@ public class UniChan : MonoBehaviour {
             Vector3 vecInput = new Vector3(0f, 0f, Input.GetAxis("Vertical"));
             if (vecInput.z > 0)
             {
-                if(Input.GetKey(KeyCode.LeftShift))
+                if (Input.GetKey(KeyCode.LeftShift))
                 {
                     shiftFlag = true;
                     addPosition.z = MOVE_Z_FRONT * 2.5f;
@@ -88,7 +139,6 @@ public class UniChan : MonoBehaviour {
                 {
                     addPosition.z = MOVE_Z_FRONT;
                 }
-                
             }
             else if (vecInput.z < 0)
             {
@@ -108,20 +158,27 @@ public class UniChan : MonoBehaviour {
             }
             
         }
+
         transform.position += ((transform.rotation * addPosition) * Time.deltaTime);
 
+        
         {
             Debug.Log(isGrounded());
-
-            if(isGrounded())
-            {
+            
+            //if(isGrounded())
+            
                 if (Input.GetButtonDown("Jump"))
                 {
-                    Rigidbody rb = GetComponent<Rigidbody>();
-                    rb.AddForce(new Vector3(0f, 5f, 0), ForceMode.VelocityChange); // 무게 고려 x 속도량, impact : 무게 고려
-                                                                                   //rb.AddForce(new Vector3(0f, 250f, 0));
-                }
-            }
+                    if (jumpFlag)
+                    {
+                        Rigidbody rb = GetComponent<Rigidbody>();
+                        rb.AddForce(new Vector3(0f, 5f, 0), ForceMode.VelocityChange); // 무게 고려 x 속도량, impact : 무게 고려
+                                                                                       //rb.AddForce(new Vector3(0f, 250f, 0));
+                                                                                       //jumpFlag = true;
+                        Animator animator = playerObject.GetComponent<Animator>();
+                        animator.SetBool("Jump", jumpFlag);
+                    }
+               }
             
         }
 
@@ -130,6 +187,7 @@ public class UniChan : MonoBehaviour {
             if(Input.GetButtonDown("Fire1"))
             {
                 shootFlag = true;
+
                 if(null != bulletStartPosition)
                 {
                     Vector3 vecBulletPos = bulletStartPosition.position;
@@ -151,13 +209,28 @@ public class UniChan : MonoBehaviour {
             animator.SetBool("Shoot", shootFlag);
             animator.SetFloat("SpeedX", addPosition.x);
             animator.SetBool("Shift", shiftFlag);
+            if(!jumpFlag)
+            {
+                animator.SetBool("Jump", jumpFlag);
+            }
         }
     }
 
     bool isGrounded()
     {
         //GameObject foot = GameObject.FindGameObjectWithTag("LeftToe");
-        GameObject foot = GameObject.Find("Character1_LeftToeBase");
-        return Physics.Raycast(foot.transform.position,Vector3.down,0.2f);    // 레이저를 쏴서 0.2f 사이에 collision이 있다면.
+        GameObject foot1 = GameObject.Find("Character1_LeftToeBase");
+        GameObject foot2 = GameObject.Find("Character1_RightToeBase");
+        return Physics.Raycast(foot1.transform.position,Vector3.down,0.2f) || Physics.Raycast(foot2.transform.position, Vector3.down, 0.2f);    // 레이저를 쏴서 0.2f 사이에 collision이 있다면.
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        jumpFlag = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        jumpFlag = false;
     }
 }
